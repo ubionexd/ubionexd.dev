2dd96d5a2df776e3fb8d83a2be3bec13c138e558
UbiOne <ubionexd@gmail.com>
2022-07-19 22:35:47 +0200

    Start rewrite
    This has functionality

---
+++ docs/about.md
@@ -0,0 +1,10 @@
+---
+title: About
+excerpt: About
+---
+
+# About
+
+This is my new blogging website.  
+I made this with the "GeekBlog" Gridsome template to save time, but i plan to highly customize this site.  
+As you can see, i'we already beggined making this look mine.
---
+++ src/pages/404.vue
@@ -0,0 +1,64 @@
+<template>
+  <Layout>
+    <div
+      class="
+        bg-gradient-to-r
+        from-secondary-300
+        to-primary-200
+        dark:from-gray-600 dark:to-gray-700
+      "
+      :style="background"
+      id="wrapper"
+    >
+      <div class="py-16 flex items-center justify-center">
+        <div
+          class="
+            bg-white
+            dark:bg-gray-700
+            shadow
+            overflow-hidden
+            sm:rounded-lg
+            pb-8
+          "
+        >
+          <div
+            class="
+              border-t border-gray-200
+              dark:border-gray-600
+              text-center
+              pt-8
+            "
+          >
+            <h1 class="text-9xl font-bold">404</h1>
+            <h1 class="text-6xl font-medium py-8">Page not found!</h1>
+            <p class="text-2xl pb-8 px-12 font-medium">
+              Oops! The page you are looking for does not exist.
+            </p>
+            <g-link
+              to="/"
+              class="
+                bg-primary-500
+                hover:bg-primary-600
+                text-white
+                font-semibold
+                hover:text-white
+                py-3
+                px-25
+                rounded-md
+                mr-6
+              "
+            >
+              Go home
+            </g-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<style>
+html {
+  background-color: #1a202d;
+}
+</style>
