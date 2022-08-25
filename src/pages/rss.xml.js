1fcd58432613f98e03f51f8cb9e0471375e2dbdd
UbiOne <ubionexd@gmail.com>
2022-08-25 22:55:16 +0200

    RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,9 @@
 import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
 export const get = () =>
 	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: "ubionexd",
+		description: "ubionexd blog and portfolio",
 		site: import.meta.env.SITE,
 		items: import.meta.glob('./blog/**/*.md'),
 	});
